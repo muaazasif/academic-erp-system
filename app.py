d4e49165bcd0f6e7ebc8df7a6e295d30b7d51924
muaazasif <muhammed.muaaz@gmail.com>
2026-04-12 15:44:27 +0500

    Fix: Add missing json import

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime, timezone
 import os
+import json
 from dotenv import load_dotenv
 import pickle
 import os.path
